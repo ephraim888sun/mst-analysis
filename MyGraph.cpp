1faf4924cf20dc8bb2e8890d1b241729b8dac2bb
ephraim888sun <ephraim888sun@gmail.com>
2023-10-25 17:08:18 -0500

    implement task1

--- MyGraph.cpp
+++ MyGraph.cpp
@@ -20,18 +20,86 @@ MyGraph::MyGraph(const MyGraph&)
 
 bool MyGraph::addEdge(int a, int b, float w)
 {
+   if (b < a)
+   {
+      int temp = a;
+      a = b;
+      b = temp;
+   }
+   if (a > numOfNodes || b > numOfNodes)
+      return false;
+   sortedList.push_back(Link{a, b, w});
    return true;
 }
 
 void MyGraph::output(ostream& os)
 {
+   os << numOfNodes << "\n";
+   for (auto l : sortedList)
+   {
+      os << l.v1 << " " << l.v2 << " " << l.w << "\n";
+   }
 }
 
 pair<bool, float> MyGraph::weight(int a, int b)
 {
-   pair<bool, float> res;
+   pair<bool, float> res = {false, -1};
+   if (b < a)
+   {
+      int temp = a;
+      a = b;
+      b = temp;
+   }
+   for (auto node : sortedList)
+   {
+      if (node.v1 == a && node.v2 == b)
+      {
+         res.first = true;
+         res.second = node.w;
+         break;
+      }
+   }
    return res;
+}
 
+void MyGraph::sortGraph()
+{
+   sort(sortedList.begin(), sortedList.end());
+}
+
+int MyGraph::findRoot(vector<int> &parent, int node)
+{
+   while (-1 != parent[node])
+   {
+      node = parent[node];
+   }
+   return node;
+}
+
+vector<Link> MyGraph::findMinimumSpanningTree(MyHelper &mh)
+{
+   sortGraph();
+   mh.sortedList = sortedList;
+   vector<Link> mst;
+   vector<int> parent(numOfNodes + 1, -1);
+   int counter = 0;
+   for (const Link &edge : sortedList)
+   {
+      int rootA = findRoot(parent, edge.v1);
+      int rootB = findRoot(parent, edge.v2);
+      if (rootA != rootB)
+      {
+         counter++;
+         mst.push_back(edge);
+         parent[rootA] = rootB;
+         if (counter >= numOfNodes - 1)
+         {
+            break;
+         }
+      }
+   }
+   mh.MST = mst;
+   return mst;
 }
 
 MyHelper::MyHelper()
@@ -40,8 +108,12 @@ MyHelper::MyHelper()
 
 vector<Link> Task1(int n, vector<Link>& pipes, MyHelper& helper)
 {
-   vector<Link> res = pipes;
-   return res;
+   MyGraph graph(n);
+   for (auto a : pipes)
+   {
+      graph.addEdge(a.v1, a.v2, a.w);
+   }
+   return graph.findMinimumSpanningTree(helper);
 }
 
 pair<bool, Link> Task2(int n, vector<Link>& pipes, Link newPipe, MyHelper helper)
--- MyGraph.h
+++ MyGraph.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-
+#ifndef MYGRAPH_H
+#define MYGRAPH_H
 using namespace std;
 
 struct Link {
@@ -9,29 +10,52 @@ public:
 
    int v1, v2;
    float w;
+
+   bool operator<(const Link &other) const
+   {
+      return w < other.w;
+   }
+   bool operator==(const Link &other) const
+   {
+      return v1 == other.v1 && v2 == other.v2 && w == other.w;
+   }
 };
 
-ostream& operator<<(ostream&, const Link&);
+class MyHelper
+{
 
+public:
+   MyHelper();
+   vector<Link> sortedList;
+   vector<Link> MST;
+   int numOfNodes;
+};
+
+ostream &operator<<(ostream &, const Link &);
 class MyGraph {
+
+   int numOfNodes;
+   vector<Link> sortedList;
 
 public:
    MyGraph();
+
+   MyGraph(int numOfNodes)
+       : numOfNodes(numOfNodes){};
+
    MyGraph(const MyGraph&);
 
    bool addEdge(int a, int b, float w);
    void output(ostream& os);
    pair<bool, float> weight(int a, int b);
 
-
-};
-
-class MyHelper {
-
-public:
-    MyHelper();
-    int x;
+   // additional helper functions
+   void sortGraph();
+   vector<Link> findMinimumSpanningTree(MyHelper &m);
+   int findRoot(vector<int> &parent, int node);
 };
 
 vector<Link> Task1(int n, vector<Link>& pipes, MyHelper& helper);
 pair<bool, Link> Task2(int n, vector<Link>& pipes, Link newPipe, MyHelper helper);
+
+#endif
